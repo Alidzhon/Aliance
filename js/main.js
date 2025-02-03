63a9c4a7a3ff646248236443e29322364d77667a
Alidzhon <khosabekovalidzhon@gmail.com>
2025-02-03 20:09:48 +0300

    fix: add swiper js

--- js/main.js
+++ js/main.js
@@ -3,8 +3,6 @@ const logo = document.querySelector(".logo-svg use");
 const mMenuToggle = document.querySelector(".mobile-menu-toggle");
 const menu = document.querySelector(".mobile-menu");
 
-
-
 const lightModeOn = (event) => {
     navbar.classList.add('navbar-light');
     logo.href.baseVal = "img/icon/sprite.svg#logo";
@@ -37,4 +35,34 @@ window.addEventListener('scroll', () => {
 mMenuToggle.addEventListener("click", (event) => {
          event.preventDefault();
     menu.classList.contains("is-open") ? closeMenu() : openMenu(); 
+});
+
+
+const swiper = new Swiper('.swiper', {
+    speed: 400,
+    autoHeight: true,
+    slidesPerView: 1,
+    navigation: {
+        nextEL: ".slider-button-next",
+        prevEL: ".slider-button-prev",
+    },
+    breakpoints: {
+        // when window width is >= 320px
+        576: {
+          slidesPerView: 2,
+        //   centeredSlides: true,
+        //   slidesOffsetBefore: -100,
+        },
+        // when window width is >= 480px
+        768: {
+          slidesPerView: 3,
+        },
+        // when window width is >= 640px
+        1024: {
+          slidesPerView: 4,
+        },
+        1200: {
+            slidesPerView: 5,
+        },
+    },
 });
