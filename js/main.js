1109e2993846e549fa3faf215614f763265d38c0
Alidzhon <khosabekovalidzhon@gmail.com>
2025-02-24 23:23:32 +0300

    refactor modal

--- js/main.js
+++ js/main.js
@@ -128,30 +128,51 @@ breakpoints: {
 });
 
 const modal = document.querySelector(".modal");
-const modalToggle = document.querySelectorAll("[data-toggle=modal]");
-const modalClose = document.querySelector(".modal-close");
+const modalDialog = document.querySelector(".modal-dialog");
 
-modalToggle.forEach((element) => {
-       element.addEventListener("click", (event) =>  {
-             event.preventDefault();
-             modal.classList.add("is-open");
-       });
+document.addEventListener("click", (event) => {
+       if (event.target.dataset.toggle == "modal" || 
+           event.target.parentNode.dataset.toggle == "modal" ||
+           (!event.composedPath().includes(modalDialog) &&
+             modal.classList.contains("is-open"))
+          ) {
+              event.preventDefault();
+              modal.classList.toggle("is-open");
+          }  
 });
 
-modalClose.addEventListener("click", (event) => {
-          event.preventDefault();
-          modal.classList.remove("is-open");
+document.addEventListener("keyup", (event) => {
+        if (event.key == "Escape" && modal.classList.contains("is-open")) {
+            modal.classList.toggle("is-open");
+        }
 });
 
-modal.addEventListener("click", (event) => {
-      const target = event.target;
-      target.classList.remove("is-open");
-});
+// const modalToggle = document.querySelectorAll("[data-toggle=modal]");
+// const modalClose = document.querySelector(".modal-close");
 
-document.addEventListener("keydown", (event) => {
-       if (event.keyCode == 27) {
-        modal.classList.remove("is-open");
-}});
+// modalToggle.forEach((element) => {
+//        element.addEventListener("click", (event) =>  {
+//              event.preventDefault();
+//              modal.classList.add("is-open");
+//        });
+// });
+
+// modalClose.addEventListener("click", (event) => {
+//           event.preventDefault();
+//           modal.classList.remove("is-open");
+// });
+
+// modal.addEventListener("click", (event) => {
+//       const target = event.target;
+//       target.classList.remove("is-open");
+// });
+
+// document.addEventListener("keydown", (event) => {
+//        if (event.keyCode == 27) {
+//         modal.classList.remove("is-open");
+// }});
+
+
 
 // const swiper2 = new Swiper('.swiper2', {
 //     speed: 400,
